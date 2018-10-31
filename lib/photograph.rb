b61443766e4a4ec3319356d1b4ec811aa23f10fe
Brad Niedt <bradleyniedt@gmail.com>
2018-10-31 09:17:13 -0600

    adds tests for the attr readers for photograph

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,6 +1,13 @@
 class Photograph
+  attr_reader :id,
+              :name,
+              :artist_id,
+              :year
   def initialize(data)
-    
+    @id = data[:id]
+    @name = data[:name]
+    @artist_id = data[:artist]
+    @year = data[:year]
   end
 
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,14 +4,33 @@ require './lib/photograph'
 require './lib/artist'
 
 class PhotographTest < Minitest::Test
-  def test_it_exists
+  def setup
     attributes = {
                   id: "1",
                   name: "Rue Mouffetard, Paris (Boy with Bottles)",
                   artist_id: "4",
                   year: "1954"
                   }
-    photograph = Photograph.new(attributes)
-    assert_instance_of Photograph, photograph
+    @photograph = Photograph.new(attributes)
+  end
+
+  def test_it_exists
+    assert_instance_of Photograph, @photograph
+  end
+
+  def test_it_has_an_id
+    assert_equal '1', @photograph.id
+  end
+
+  def test_it_has_a_name
+    assert_equal 'Rue Mouffetard, Paris (Boy with Bottles)', @photograph.name
+  end
+
+  def test_it_has_an_artist_id
+    assert_equal '4', @photograph.artist_id
+  end
+
+  def test_it_has_a_year
+    assert_equal '1954', @photograph.year
   end
 end
