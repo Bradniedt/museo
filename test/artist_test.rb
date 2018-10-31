eb89845f09541d2ad1d5f3a5f8bc77b0134f8a49
Brad Niedt <bradleyniedt@gmail.com>
2018-10-31 09:25:06 -0600

    adds tests for artist attr readers

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -19,5 +19,20 @@ class ArtistTest < Minitest::Test
     assert_instance_of Artist, @artist
   end
 
+  def test_it_has_an_id
+    assert_equal '2', @artist.id
+  end
+
+  def test_it_has_a_name
+    assert_equal 'Ansel Adams', @artist.name
+  end
+
+  def test_it_has_a_birth_date
+    assert_equal '1902', @artist.born
+  end
+
+  def test_it_has_a_death_date
+    assert_equal '1984', @artist.died
+  end
 
 end
