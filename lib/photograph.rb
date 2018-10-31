e346f0d4d749b845848ad506d260ff9a20b4c1be
Brad Niedt <bradleyniedt@gmail.com>
2018-10-31 09:17:50 -0600

    attr reader tests passing

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -6,7 +6,7 @@ class Photograph
   def initialize(data)
     @id = data[:id]
     @name = data[:name]
-    @artist_id = data[:artist]
+    @artist_id = data[:artist_id]
     @year = data[:year]
   end
 
